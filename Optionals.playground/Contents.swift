6cd2a139564ec6e9eaef691401705eb20b61cdb7
Ahmed <ahmed.hussein@zarcony.dev>
2023-05-28 11:29:56 +0300

    Optionals

---
+++ Optionals.playground/Contents.swift
@@ -0,0 +1,172 @@
+import Foundation
+
+func multiplyByTwo(_ value: Int? = nil) -> Int {
+    if let value {
+        return value * 2
+    } else {
+        return 0
+    }
+}
+
+multiplyByTwo()
+multiplyByTwo(nil)
+multiplyByTwo(2)
+multiplyByTwo(8)
+
+
+let age: Int? = nil
+if age != nil {
+    "Age is there! How odd!"
+} else {
+    "Age is nil. Correct."
+}
+
+if let age {
+    "Age is there. How odd! Its value is \(age)"
+} else {
+    "No age is present. As expected"
+}
+
+func checkAge() {
+    if age == nil {
+        "Age is nil as expected"
+        return
+    }
+    
+    guard age != nil else {
+        "Age is nil as expected"
+        return
+    }
+}
+
+
+let age2: Int? = 0
+func checkAge2() {
+    guard let age2 else {
+        "Age is nil"
+        return
+    }
+    "Age value \(age2)"
+}
+
+switch age {
+case .none:
+    "Age has no value as expected"
+    break
+case let .some(value):
+    "Age has the value of \(value)"
+    break
+}
+
+if age == 0 {
+    "zero"
+} else {
+    "No zero"
+}
+
+if age == .some(0) {
+    "zero"
+} else {
+    "No zero"
+}
+
+struct Person {
+    let name: String
+    let address: Address?
+    struct Address {
+        let firstLine: String?
+    }
+}
+
+let foo: Person = Person (
+    name: "Foo",
+    address: nil
+)
+
+if let fooFirstAddressLine =
+    foo.address?.firstLine {
+    fooFirstAddressLine
+} else {
+    "no address"
+}
+
+if let fooAddress = foo.address,
+   let firstLine = fooAddress.firstLine {
+    fooAddress
+    firstLine
+}
+
+
+let bar: Person? = Person(
+    name: "Bar",
+    address: Person.Address(
+        firstLine: nil
+    )
+)
+
+if bar?.name == "Bar",
+   bar?.address?.firstLine == nil {
+    "Bar name withour firstline"
+} else {
+    "Seems like something isn't working right!"
+}
+
+
+let baz: Person? = Person(
+    name: "Baz",
+    address: Person.Address(
+        firstLine: "Baz first line"
+    )
+)
+
+switch baz?.address?.firstLine {
+case let .some(firstLine) where firstLine.starts(with: "Baz"):
+    "Baz first address line = \(firstLine)"
+case let .some(firstLine):
+    "Baz first addres line thatn didn't match the previous case"
+    firstLine
+case .none:
+    "Baz first addres line is nil?? Weird!"
+}
+
+func getFullName(
+    firstName: String,
+    lastName: String?
+) -> String? {
+    if let lastName {
+        return "\(firstName) \(lastName)"
+    } else {
+        return nil
+    }
+}
+
+getFullName(
+    firstName: "Foo",
+    lastName: nil
+)
+
+getFullName(
+    firstName: "Foo",
+    lastName: "Bar"
+)
+
+
+func getFullName2(
+    firstName: String,
+    lastName: String?
+) -> String? {
+    guard let lastName else {
+        return nil
+    }
+    return "\(firstName) \(lastName)"
+}
+
+getFullName2(
+    firstName: "Foo",
+    lastName: nil
+)
+
+getFullName2(
+    firstName: "Foo",
+    lastName: "Bar"
+)
